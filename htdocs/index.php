bad544521bf41705d0e8b624cfcaded7e812f90b
yamahare <jidai.norio@gmail.com>
2018-01-07 22:44:32 +0900

    Index.phpをコントローラ役に変更。aboutページ追加。

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,9 +3,19 @@ require_once('../vendor/autoload.php');
 require_once('../config/DBConfig.php');
 require_once('../model/ModelBase.php');
 
-$magazine = new Magazine;
-$magazine_last_update  = $magazine->get_magazine_last_update();
-$magazine_current_next = $magazine->get_magazine_current_next();
+//先頭のスラッシュを削除
+$req_uri = ltrim($_SERVER['REQUEST_URI'], '/');
+
+$params = array();
+if ('' != $req_uri) {
+    // パラメーターを"/"で分割
+    $params = explode('/', $req_uri);
+}
+// １番目のパラメーターをコントローラーとして取得
+$controller = 'index';
+if (0 < count($params)) {
+    $controller = $params[0];
+}
 
 //---------------------------------------
 //        twigの設定
@@ -17,11 +27,33 @@ $twig = new Twig_Environment($loader, array(
     'debug' => true,
 ));
 $twig->addExtension(new Twig_Extension_Debug());
-$template = $twig->load('index.html.twig');
 
-$data = array(
-    'message' => $magazine_last_update,
-    'array'   => $magazine_current_next,
-);
+switch ($controller){
+    case 'index':
+        $template = $twig->load('index.html.twig');
 
-echo $template->render($data);
+        $magazine = new Magazine;
+        $magazine_last_update  = $magazine->get_magazine_last_update();
+        $magazine_current_next = $magazine->get_magazine_current_next();
+
+        $data = array(
+            'page' => 'index',
+            'message' => $magazine_last_update,
+            'array'   => $magazine_current_next,
+        );
+
+        echo $template->render($data);
+        break;
+
+    case 'about':
+        $template = $twig->load('about.html.twig');
+
+        $data = array(
+            'page' => 'about',
+        );
+
+        echo $template->render($data);
+        break;
+    default:
+        echo '誰だお前';
+}
