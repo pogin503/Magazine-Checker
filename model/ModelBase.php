f3091f5573262a206803dfe0dae4582f28c41b39
yamahare <jidai.norio@gmail.com>
2018-01-21 20:14:39 +0900

    前号と次号を取得するSQLをViewを使用しないように変更（クエリをキャッシュするため）

--- model/ModelBase.php
+++ model/ModelBase.php
@@ -53,9 +53,47 @@ class Magazine extends ModelBase{
         $array = [];
         $today = new DateTime('now');
         $today_format = date_format($today, 'Y/m/d H:i:s');
+        $curDate = date("Y-m-d", time());
 
-        $sql = "SELECT *
-                FROM current_next_v
+        //クエリをキャッシュするためにViewをやめる
+        $sql = "SELECT  MN.name
+                       ,MN.url
+                       ,MN.status
+                       ,UNI.title
+                       ,UNI.release_date
+                FROM   (
+                       -- 次号
+                       SELECT   TAR.magazine_id
+                               ,TAR.title
+                               ,TAR.release_date
+                       FROM     titles_and_release_date TAR
+                                INNER JOIN (SELECT   magazine_id
+                                                    ,MIN(release_date) AS min_date
+                                            FROM     titles_and_release_date
+                                            WHERE    release_date >= '$curDate'
+                                            GROUP BY magazine_id
+                                          ) AS MIN_TABLE
+                                      ON  TAR.magazine_id  = MIN_TABLE.magazine_id
+                                      AND TAR.release_date = MIN_TABLE.min_date
+                       UNION ALL
+                       -- 今号
+                       SELECT  TAR.magazine_id
+                              ,TAR.title
+                              ,TAR.release_date
+                       FROM   titles_and_release_date TAR
+                              INNER JOIN (SELECT   magazine_id
+                                                  ,MAX(release_date) AS max_date
+                                          FROM     titles_and_release_date
+                                          WHERE    release_date <= '$curDate'
+                                          GROUP BY magazine_id
+                                         ) AS MAX_TABLE
+                                    ON  TAR.magazine_id  = MAX_TABLE.magazine_id
+                                    AND TAR.release_date = MAX_TABLE.max_date
+                       ) AS UNI
+                       INNER JOIN magazines MN
+                             ON   UNI.magazine_id = MN.id
+                ORDER BY  MN.name
+                        ,UNI.release_date DESC
                 ";
         $stmt = $this->db->query($sql);
         $stmt -> execute();
