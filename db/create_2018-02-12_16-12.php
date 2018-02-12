1d70b5dadb994b14611f25beaa1d455fe1aa7fb7
yamahare <jidai.norio@gmail.com>
2018-02-12 18:47:36 +0900

    create文修正

--- db/create_2018-02-12_16-12.php
+++ db/create_2018-02-12_16-12.php
@@ -11,19 +11,19 @@ try{
     *  magazinesテーブル
     ======================================*/
     //magazinesテーブルがあったら削除
-    $sql = "DROP TABLE IF EXISTS `magazines`";
+    $sql = "DROP TABLE IF EXISTS magazines";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
 
     //magazinesテーブル作成
-    $sql = "CREATE TABLE `magazines` (
-              `id`         int(10) NOT NULL AUTO_INCREMENT,
-              `name`       varchar(100) COLLATE utf8_bin NOT NULL,
-              `url`        varchar(300) COLLATE utf8_bin NOT NULL,
-              `status`     tinyint(1) NOT NULL,
-              `created_at` datetime NOT NULL,
-              `updated_at` datetime NOT NULL,
-              PRIMARY KEY (`id`)
+    $sql = "CREATE TABLE magazines (
+              id         int(10)       NOT NULL AUTO_INCREMENT,
+              name       varchar(100)  NOT NULL,
+              url        varchar(300)  NOT NULL,
+              status     tinyint(1)    NOT NULL,
+              created_at datetime      NOT NULL,
+              updated_at datetime      NOT NULL,
+              PRIMARY KEY (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
@@ -33,20 +33,20 @@ try{
      ======================================*/
 
     //titles_and_release_dateテーブルがあったら削除
-    $sql = "DROP TABLE IF EXISTS `titles_and_release_date`";
+    $sql = "DROP TABLE IF EXISTS titles_and_release_date";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
 
     //titles_and_release_dateテーブル作成
-    $sql = "CREATE TABLE `titles_and_release_date` (
-              `id`            int(10) NOT NULL AUTO_INCREMENT,
-              `magazine_id`   int(11) NOT NULL,
-              `title`         varchar(100) COLLATE utf8_bin NOT NULL,
-              `del_flag`      tinyint(1) NOT NULL DEFAULT '0',
-              `release_date`  date NOT NULL,
-              `created_at`    datetime NOT NULL,
-              `updated_at`    datetime NOT NULL,
-              PRIMARY KEY (`id`)
+    $sql = "CREATE TABLE titles_and_release_date (
+              id            int(10)       NOT NULL AUTO_INCREMENT,
+              magazine_id   int(11)       NOT NULL,
+              title         varchar(100)  NOT NULL,
+              del_flag      tinyint(1)    NOT NULL DEFAULT 0,
+              release_date  date          NOT NULL,
+              created_at    datetime      NOT NULL,
+              updated_at    datetime      NOT NULL,
+              PRIMARY KEY (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
@@ -55,23 +55,23 @@ try{
     *  url_and_regexテーブル
     ======================================*/
     //url_and_regexテーブルがあったら削除
-    $sql = "DROP TABLE IF EXISTS `url_and_regex`";
+    $sql = "DROP TABLE IF EXISTS url_and_regex";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
 
     //magazinesテーブル作成
-    $sql = "CREATE TABLE `url_and_regex` (
-              `id`          int(11) NOT NULL AUTO_INCREMENT,
-              `magazine_id` int(10) NOT NULL,
-              `url`         varchar(200) COLLATE utf8_bin NOT NULL,
-              `file_path`   varchar(100) COLLATE utf8_bin NOT NULL,
-              `target`      varchar(100) COLLATE utf8_bin NOT NULL,
-              `getpicflg`   tinyint(1) DEFAULT NULL,
-              `title_reg`   varchar(100) COLLATE utf8_bin NOT NULL,
-              `release_reg` varchar(100) COLLATE utf8_bin NOT NULL,
-              `created_at`  datetime NOT NULL,
-              `updated_at`  datetime NOT NULL,
-              PRIMARY KEY (`id`)
+    $sql = "CREATE TABLE url_and_regex (
+              id          int(11)      NOT NULL AUTO_INCREMENT,
+              magazine_id int(10)      NOT NULL,
+              url         varchar(200) NOT NULL,
+              file_path   varchar(100) NOT NULL,
+              target      varchar(100) NOT NULL,
+              getpicflg   tinyint(1)   DEFAULT NULL,
+              title_reg   varchar(100) NOT NULL,
+              release_reg varchar(100) NOT NULL,
+              created_at  datetime NOT NULL,
+              updated_at  datetime NOT NULL,
+              PRIMARY KEY (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
 
     if ($model->execute($sql) !== false ){ print $sql."\n"; }
